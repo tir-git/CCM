bc7f72354c1998a7fba518086e3b84d4c3d679d2
sidiousforce <sidiousforce@naver.com>
2023-05-03 11:12:59 +0900

    modified:   main.py modified:   requirements.txt selenium 기능 추가중

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#모듈 불러오기
 import requests
+import time
+import warnings
+
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 url="https://www.classcard.net/set/13158880/950323"
 
 response=requests.get(url)
