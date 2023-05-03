7dbdc4450cfc1c3ffb1d6e58ceb51acc5adff2ed
sidiousforce <sidiousforce@naver.com>
2023-05-03 15:27:57 +0900

    modified:   main.py modified:   requirements.txt 추가적 모듈 및 기본 기능 추가

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-#모듈 불러오기
+# Import Modules
 import requests
 import time
 import warnings
+import chromedriver_autoinstaller
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 
-url="https://www.classcard.net/set/13158880/950323"
+# Chrome Driver Autoinstall & Setting
+options=webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches",["enable-logging"])
+driver=webdriver.Chrome(service=Service(chromedriver_autoinstaller.install()),options=options)
 
-response=requests.get(url)
+# Classcard URL 입력받기
+ccsite=input("자동 학습을 원하는 Classcard 사이트의 URL을 입력하세요. : ")
 
-soup=BeautifulSoup(response.text, "html.parser")
-print (soup)
+print(ccsite)
