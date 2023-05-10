9526e34d132b544153c11211ff430b164631d1ee
sidiousforce <sidiousforce@naver.com>
2023-05-10 11:06:55 +0900

    new file:   __pycache__/function.cpython-310.pyc new file:   function.py modified:   main.py
    자동 로그인 기능, 단어 세트 학습 기능 기초 구현
    안내 문구 추가

---
+++ function.py
@@ -0,0 +1,12 @@
+import requests
+from selenium import webdriver
+
+def world_learn (driver, num):
+    # num 길이의 리스트 생성 (초기값 0)
+    eng=[0 for i in range (num)]
+    kor=[0 for i in range (num)]
+    
+    for i in range (num):
+        eng[i]=driver.find_by_element_by_xpath(f"//*[@id='tab_set_all']/div[2]/div[{i}]/div[4]/div[1]/div[1]/div/div").text
+        driver.find_by_css_selector("#tab_set_all > div.card-list-title > div > div:nth-child(1) > a").click
+        
--- main.py
+++ main.py
@@ -4,18 +4,72 @@ import time
 import warnings
 import chromedriver_autoinstaller
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 
+from function import world_learn
+
 # Chrome Driver Autoinstall & Setting
-options=webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches",["enable-logging"])
-driver=webdriver.Chrome(service=Service(chromedriver_autoinstaller.install()),options=options)
+print("최적의 구동 환경을 위하여 크롬 브라우저를 자동으로 설치합니다.")
+check=input("Proceed? (Y/n) : ")
+if check=='y':
+    options=webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches",["enable-logging"])
+    driver=webdriver.Chrome()
+    driver=webdriver.Chrome(service=Service(chromedriver_autoinstaller.install()),options=options)
+else :
+    print("크롬 브라우저 설치가 거부되었습니다.")
+    print("Press any key to quit...")
+    quit()
 
-# Classcard URL 입력받기
-ccsite=input("자동 학습을 원하는 Classcard 사이트의 URL을 입력하세요. : ")
+# Classcard Login
+driver.get("https://www.classcard.net/login")
+print("\n사용자의 개인정보는 프로그램에 저장되지 않으며, 로그인 용도로만 사용됩니다.\n직접 브라우저에서 로그인 할 시 오류가 발생할 수 있습니다.\n")
+cc_id=input("클래스카드 ID를 입력하세요. : ")
+cc_pw=input("클래스카드 비밀번호를 입력하세요. : ")
+wait=WebDriverWait(driver, 10)
+tag_id=wait.until(EC.visibility_of_element_located((By.ID, "login_id")))
+tag_pwd=wait.until(EC.visibility_of_element_located((By.ID, "login_pwd")))
+tag_id.clear()
+tag_id.send_keys(cc_id)
+tag_pwd.send_keys(cc_pw)
+wait=WebDriverWait(driver, 10)
+loginbutton=wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#loginForm > div.checkbox.primary.text-primary.text-center.m-t-md > button")))
+loginbutton.click()
 
-print(ccsite)
+# Input Classcard URL 
+ccsite=input("\n자동 학습을 원하는 Classcard 사이트의 URL을 입력하세요. : ")
+
+# Check if the URL is valid
+try:
+    time.sleep(1)
+    driver.get(ccsite)
+    driver.find_element(By.XPATH, "//div[@class='p-b-sm']")
+    time.sleep(1)
+except:
+    print("\n입력한 URL이 잘못되었습니다.\n")
+    print("Press any key to quit...")
+    quit()
+
+# Classcard Vocabulary Set change range to all
+wait=WebDriverWait(driver, 10)
+rangesel=wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div.mw-1080 > div.p-b-sm > div.set-body.m-t-25.m-b-lg > div.m-b-md > div > a")))
+rangesel.click()
+rangesel1=wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div.mw-1080 > div.p-b-sm > div.set-body.m-t-25.m-b-lg > div.m-b-md > div > ul > li:nth-child(1)")))
+rangesel1.click()
+
+html=BeautifulSoup(driver.page_source, "html.parser")
+cards_ele=html.find("div", class_="flip-body")
+num=len(cards_ele.find_all("div", class_="flip-card"))
+
+
+
+
+
